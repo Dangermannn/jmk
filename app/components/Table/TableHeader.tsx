dab23df77d7034fbe74db4e10c41e91f72c53f06
Dangermannn <dangermanplyt@gmail.com>
2025-07-29 15:53:31 +0200

    adjust tables

--- app/components/Table/TableHeader.tsx
+++ app/components/Table/TableHeader.tsx
@@ -2,7 +2,7 @@ export const TableHeader: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
   return (
-    <thead className="text-base text-gray-700 uppercase bg-gray-50">
+    <thead className="text-base text-gray-700 uppercase bg-gray-50 bg-gray-300 text-gray-100">
       <tr>{children}</tr>
     </thead>
   );
