ca4de5daa3316f714335f580b9212c33bc323b32
Dangermannn <dangermanplyt@gmail.com>
2025-07-29 15:56:39 +0200

    fix rounded table

--- app/components/Table/Table.tsx
+++ app/components/Table/Table.tsx
@@ -2,7 +2,7 @@ export const Table: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
   return (
-    <div className="my-4 w-full max-w-full bg-white shadow-md sm:rounded-lg overflow-hidden">
+    <div className="my-4 w-full max-w-full bg-white shadow-md rounded-lg overflow-hidden">
       <table className="table-auto w-full text-base text-left">
         {children}
       </table>
